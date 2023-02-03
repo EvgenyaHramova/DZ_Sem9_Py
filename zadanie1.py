95026c9676e2ed7e09390104020f02523507aac5
EvgenyaHramova <evgenya.hramova@yandex.ru>
2023-02-03 21:39:18 +0400

    Z1

---
+++ zadanie1.py
@@ -0,0 +1,21 @@
+# from isOdd import isOdd
+# print(isOdd('1'))
+# print(isOdd('5'))
+
+# print(isOdd(1))
+# print(isOdd(4))
+
+# from progress.bar import Bar
+# import time
+
+# bar = Bar('Processing', max=20)
+# for i in range(20):
+#     time.sleep(1) # задержка выполнения в 1 секундуб чтобы имитировать процесс скачивания
+#     # Do some work
+#     bar.next()
+# bar.finish()
+
+
+# import emoji
+# print(emoji.emojize('Python is :thumbs_up:'))
+
