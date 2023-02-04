e82c0e3095a68326c932976307891d43f65fd4fb
EvgenyaHramova <evgenya.hramova@yandex.ru>
2023-02-04 16:58:31 +0400

    bc

--- bot_commands.py
+++ bot_commands.py
@@ -8,6 +8,7 @@ async def hello_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(update, context)
     await update.message.reply_text(f'Hello, darling {update.effective_user.first_name}')
 
+
 async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(update, context)
     await update.message.reply_text(f'{datetime.datetime.now().time()}')
